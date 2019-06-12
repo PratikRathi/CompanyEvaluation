ed497eea129d30d56aa119b01a23680b2f8d197b
PratikRathi <pratikrathi14@gmail.com>
2019-06-12 13:00:01 +0530

    views

--- front_page/views.py
+++ front_page/views.py
@@ -37,7 +37,7 @@ def ml_index(request):
     print("------------")
     print(df.head()) 
     X_test = df
-    loaded_model = pickle.load(open('/home/rahil/Desktop/FInal Project/finalized_model_7class.sav', 'rb'))
+    loaded_model = pickle.load(open('/home/pratik/Desktop/Technical/finalized_model_7class.sav', 'rb'))
     y_pred=loaded_model.predict(X_test)
 
     df[0]
@@ -98,24 +98,26 @@ def excel_index(request):
         investments = []
         current_assets = []
         receivables = []
+        years = []
 
         max_column = df.max_column
-        for i in range(1, max_column + 1):
-            shareholders.append(df.cell(row=6, column=i))
-            Total_Assets.append(df.cell(row=35, column=i))
-            Total_Liabilities.append(df.cell(15, i))
-            cash.append(df.cell(32, i))
-            investments.append(df.cell(29, i))
-            current_assets.append(df.cell(34, i))
-            receivables.append(df.cell(31, i))
+        for i in range(1,max_column + 1):
+            years.append(df.cell(row = 1,column = i).value)
+            shareholders.append(df.cell(row = 7,column = i))
+            Total_Assets.append(df.cell(row = 36,column = i))
+            Total_Liabilities.append(df.cell(16, i))
+            cash.append(df.cell(33, i))
+            investments.append(df.cell(30, i))
+            current_assets.append(df.cell(35, i))
+            receivables.append(df.cell(32, i))
 
         shareholders = list(shareholders)
         Total_Assets = list(Total_Assets)
-        # Total_Liabilities = list(df.iloc[14])
-        # cash = list(df.iloc[31])
-        # investments = list(df.iloc[28])
-        # current_assets = list(df.iloc[33])
-        # receivables = list(df.iloc[30])
+        #Total_Liabilities = list(df.iloc[14])
+        #cash = list(df.iloc[31])
+        #investments = list(df.iloc[28])
+        #current_assets = list(df.iloc[33])
+        #receivables = list(df.iloc[30])
 
         cash.pop(0)
         investments.pop(0)
@@ -129,15 +131,22 @@ def excel_index(request):
         print(x_quick)
         for i in range(0, len(Total_Liabilities)):
             current_ratio.append(float(Total_Assets[i].value) / float(Total_Liabilities[i].value))
-            x_quick[i] = float(cash[i].value) + float(investments[i].value) + float(current_assets[i].value) + float(
-                receivables[i].value)
+            x_quick[i] = float(cash[i].value) + float(investments[i].value) + float(current_assets[i].value) + float(receivables[i].value)
             quick_ratio.append(float(x_quick[i]) / float(Total_Liabilities[i].value))
             debt_equity.append(float(Total_Liabilities[i].value) / float(shareholders[i].value))
         print(current_ratio)
         print(quick_ratio)
 
+        #df.loc[len(df)] = current_ratio
+        #df.loc[len(df) + 1] = quick_ratio
+        #df.loc[len(df) + 2] = debt_equity
         excel_data = list()
-
+        #excel_data1 = list()
+        #for row in df.iter_rows():
+         #   row_data = list()
+          #  for cell in row:
+           #     row_data.append(str(cell.value))
+            #excel_data1.append(row_data)
         excel_data.append(current_ratio)
         excel_data.append(quick_ratio)
         excel_data.append(debt_equity)
@@ -150,14 +159,16 @@ def excel_index(request):
 
         print(df2.max_column)
         l3 = []
-        current_ratio_perc = []
         l3.append(['current_ratio', 'quick_ratio', 'debt_equity'])
         excel_data = list(excel_data)
+        current_ratio_perc = []
         for i in range(1, df2.max_column - 1):
             l = []
             l1 = []
-            [l.append(excel_data[p][i]) for p in range(0, 3)]
-            [l1.append(excel_data[k][i + 1]) for k in range(0, 3)]
+            #l = [df2.cell(row=k, column=i) for k in range(0, 3)]
+            [ l.append(excel_data[p][i]) for p in range(0,3) ]
+            [ l1.append(excel_data[k][i+1]) for k in range(0, 3)]
+            #l1 = [df2.cell(row=k, column=i + 1) for k in range(0, 3)]
             for j in range(0, len(l)):
                 if (l1[j] == 0 or l[j] == 0):
                     l[j] = 0
@@ -166,33 +177,27 @@ def excel_index(request):
             l3.append(l)
             current_ratio_perc = [[l3[j][i] for j in range(len(l3))] for i in range(len(l3[0]))]
 
+            ratio_data = {"current_ratio": [], "quick_ratio": [], "debt_equity": [], "current_ratio_perc": []}
+            i = 1
+            name = 0
 
+            # we have a number of lists in excel_data and this function is
+            # converting it into lists of each key based on list number
+            for row in excel_data:
+                i = 0
+                name = name + 1
+                for cell in row:
+                    if (i == 0):
+                        i = i + 1
+                    elif (name == 1):
+                        ratio_data['current_ratio'].append(cell)
+                    elif (name == 2):
+                        ratio_data['quick_ratio'].append(cell)
+                    elif (name == 3):
+                        ratio_data['debt_equity'].append(cell)
+            for row in current_ratio_perc:
+                ratio_data['current_ratio_perc'].append(row)
 
-
-        # made a dictionary and added key name with a list value
-        ratio_data = {"current_ratio": [], "quick_ratio": [], "debt_equity": [],"current_ratio_perc" : []}
-        i=1
-        name = 0
-
-        # we have a number of lists in excel_data and this function is
-        # converting it into lists of each key based on list number
-        for row in excel_data:
-            i=0
-            name=name+1
-            for cell in row:
-               if(i==0):
-                   i=i+1
-               elif(name==1):
-                   ratio_data['current_ratio'].append(cell)
-               elif(name==2):
-                   ratio_data['quick_ratio'].append(cell)
-               elif (name==3):
-                   ratio_data['debt_equity'].append(cell)
-        for row in current_ratio_perc:
-            ratio_data['current_ratio_perc'].append(row)
-
-        # passing a dictionary to the template
-        return render(request, 'excel.html', ratio_data)
-
-
+            # passing a dictionary to the template
+            return render(request, 'excel.html',  ratio_data)
 
